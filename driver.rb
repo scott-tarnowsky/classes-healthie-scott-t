f19782c3e491b8be5a2b7fc6f45c2b7b3ca7ad2a
rabbledoo <scott.tarnowsky@gmail.com>
2024-12-25 12:25:45 -0500

    added arg parser/command line driver to run program

--- driver.rb
+++ driver.rb
@@ -1,2 +1,39 @@
 require_relative 'food'
 require_relative 'animal'
+
+if ARGV.length != 2
+  puts "Usage: ruby main.rb [dog|cat] [chicken|cat_food|dog_food|human_food|lemon|milk]"
+  exit 1
+end
+
+animal_type, food_type = ARGV
+
+food = case food_type.downcase
+when "chicken"
+  Chicken.new("chicken")
+when "cat_food"
+  CatFood.new("cat food")
+when "dog_food"
+  DogFood.new("dog food")
+when "human_food"
+  HumanFood.new("human food")
+when "lemon"
+  Lemon.new("lemon")
+when "milk"
+  Milk.new("milk")
+else
+  puts "Invalid food type"
+  exit 1
+end
+
+animal = case animal_type.downcase
+when "dog"
+  Dog.new
+when "cat"
+  Cat.new
+else
+  puts "Invalid animal type. Please choose 'dog' or 'cat'"
+  exit 1
+end
+
+puts animal.eat(food)
